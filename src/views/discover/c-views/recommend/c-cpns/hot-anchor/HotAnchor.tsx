61cdda49a263b0772c4173be7bcdf9f987ee513e
bubble <1065046544@qq.com>
2023-05-05 11:39:09 +0800

    热门主播的搭建

--- src/views/discover/c-views/recommend/c-cpns/hot-anchor/HotAnchor.tsx
+++ src/views/discover/c-views/recommend/c-cpns/hot-anchor/HotAnchor.tsx
@@ -2,6 +2,8 @@ import React, { memo } from 'react'
 import type { ReactNode, FC } from 'react'
 import { HotAnchorWrapper } from './style'
 import SectionHeaderV2 from '@/components/section-header-v2/SectionHeaderV2'
+import { hotRadios } from '@/assets/data/local_data'
+import { formatImageToSize } from '@/utils/format'
 interface IProps {
   children?: ReactNode
 }
@@ -10,7 +12,23 @@ const HotAnchor: FC<IProps> = memo((props: IProps) => {
   return (
     <HotAnchorWrapper>
       <SectionHeaderV2 title="热门主播" />
-      <h2>HotAnchor</h2>
+      <div className="hotdj-list">
+        {hotRadios.map((item) => {
+          return (
+            <div className="hotdj-item" key={item.name}>
+              <a href="" className="cover">
+                <img src={formatImageToSize(item.picUrl, 40)} alt="" />
+              </a>
+              <div className="info">
+                <p className="nickname">
+                  <a href="">{item.name}</a>
+                </p>
+                <p className="position">{item.position}</p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </HotAnchorWrapper>
   )
 })
--- src/views/discover/c-views/recommend/c-cpns/hot-anchor/style.ts
+++ src/views/discover/c-views/recommend/c-cpns/hot-anchor/style.ts
@@ -2,4 +2,40 @@ import styled from 'styled-components'
 
 export const HotAnchorWrapper = styled.div`
   margin-top: 15px;
+  .hotdj-list {
+    margin: 20px 0 0 20px;
+
+    .hotdj-item {
+      display: flex;
+      align-items: center;
+      width: 210px;
+      height: 50px;
+      .cover {
+        width: 40px;
+        margin-right: 10px;
+        > img {
+          width: 40px;
+          height: 40px;
+          box-shadow: 0 0 1px #333333 inset;
+        }
+      }
+      .info {
+        width: 160px;
+        line-height: 21px;
+        .nickname {
+          width: 100%;
+          > a {
+            font-size: 12px;
+          }
+        }
+        .position {
+          width: 100%;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          word-wrap: normal;
+        }
+      }
+    }
+  }
 `
